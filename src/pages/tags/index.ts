22f25d315bd6cd91fa422cafa28e0d912feea7f6
wenxue.cwx <coolman_84@163.com>
2020-12-07 22:02:44 +0800

    add tags

---
+++ src/pages/tags/index.ts
@@ -0,0 +1,21 @@
+import $ from 'jquery';
+import 'prismjs';
+import 'prismjs/themes/prism-tomorrow.css';
+
+import 'prismjs/plugins/toolbar/prism-toolbar';
+import 'prismjs/plugins/toolbar/prism-toolbar.css';
+
+import 'prismjs/plugins/line-numbers/prism-line-numbers';
+import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
+
+import 'prismjs/plugins/show-language/prism-show-language';
+
+import 'prismjs/plugins/copy-to-clipboard/prism-copy-to-clipboard';
+
+import './index.scss';
+
+import '@assets/scripts/scroll';
+
+$(() => {
+  console.log('index');
+});
